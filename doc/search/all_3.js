6328b5c2faacd6906d99c8fd42fb13dc7c776417
Daniel Verkamp <daniel.verkamp@intel.com>
2016-11-07 13:17:42 -0700

    gh-pages/doc: update to 6583441
    Signed-off-by: Daniel Verkamp <daniel.verkamp@intel.com>

--- doc/search/all_3.js
+++ doc/search/all_3.js
@@ -4,8 +4,8 @@ var searchData=
   ['destruct',['destruct',['../structspdk__bdev__fn__table.html#a724dab527d4b6be934fc8423f22bd8cb',1,'spdk_bdev_fn_table']]],
   ['detach',['detach',['../structspdk__nvmf__ctrlr__ops.html#a75f0dbfdd3001a9502af51ab8fbe6037',1,'spdk_nvmf_ctrlr_ops']]],
   ['dev',['dev',['../structspdk__scsi__lun.html#a8bbbbb03b32ddd899d51e928efeb1722',1,'spdk_scsi_lun']]],
-  ['dpc',['dpc',['../structspdk__nvme__ns__data.html#af67d267b2af2a901505b2696c535d5eb',1,'spdk_nvme_ns_data']]],
-  ['dps',['dps',['../structspdk__nvme__ns__data.html#aae00d2b1701a8b23ed34d2049dba9fe9',1,'spdk_nvme_ns_data']]],
+  ['dpc',['dpc',['../structspdk__nvme__ns__data.html#af0c0feee445a2a81afa08b2e047da033',1,'spdk_nvme_ns_data']]],
+  ['dps',['dps',['../structspdk__nvme__ns__data.html#a087d73c72dc68d3717c78aa8c3132db9',1,'spdk_nvme_ns_data']]],
   ['driver_5fctx',['driver_ctx',['../structspdk__bdev__io.html#ab1b334589d89261c8e0242fee614caa7',1,'spdk_bdev_io']]],
   ['dstrd',['dstrd',['../unionspdk__nvme__cap__register.html#af726e8465f09c36be0185475ec67fbce',1,'spdk_nvme_cap_register']]]
 ];
--- doc/search/variables_d.js
+++ doc/search/variables_d.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['oacs',['oacs',['../structspdk__nvme__ctrlr__data.html#a2c5fe8ae32bcaf5bb54ca89b5f81714f',1,'spdk_nvme_ctrlr_data']]],
-  ['oaes',['oaes',['../structspdk__nvme__ctrlr__data.html#afb5c885995ab99fc479f0826254f807e',1,'spdk_nvme_ctrlr_data']]],
+  ['oacs',['oacs',['../structspdk__nvme__ctrlr__data.html#a7c42e56f0096411d72f929f3b551fc9a',1,'spdk_nvme_ctrlr_data']]],
+  ['oaes',['oaes',['../structspdk__nvme__ctrlr__data.html#a883e0a3d2265d684030777a71b05333b',1,'spdk_nvme_ctrlr_data']]],
   ['offset',['offset',['../structspdk__bdev__io.html#ac36ecea8bcf4d82a2cd8ced099c6d41b',1,'spdk_bdev_io']]],
   ['ofst',['ofst',['../unionspdk__nvme__cmbloc__register.html#a12f58ebd196434ac7768545fde9c3340',1,'spdk_nvme_cmbloc_register']]],
-  ['oncs',['oncs',['../structspdk__nvme__ctrlr__data.html#ae899b1982c9621802305ddb9de99b5f1',1,'spdk_nvme_ctrlr_data']]]
+  ['oncs',['oncs',['../structspdk__nvme__ctrlr__data.html#aa3f9eeb48e7748ffad1a292557899d85',1,'spdk_nvme_ctrlr_data']]]
 ];
